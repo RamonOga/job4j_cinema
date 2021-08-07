167007735259759112bd1cd6e998156f081feeb5
Ramon Markelov <Roman.sercent@gmail.com>
2021-08-07 22:35:39 +0300

    repair payment.html

--- src/main/ru/cinema/servlet/HallServlet.java
+++ src/main/ru/cinema/servlet/HallServlet.java
@@ -3,6 +3,7 @@ package ru.cinema.servlet;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,24 +22,19 @@ public class HallServlet extends HttpServlet {
     private static final Gson GSON = new GsonBuilder().create();
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String tmp = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        String tmp = req.getReader()
+                .lines()
+                .collect(Collectors
+                        .joining(System.lineSeparator()));
         System.out.println(tmp);
-        //String input = GSON.fromJson(req.getReader(), String.class);
-        //System.out.println(input);
         System.out.println("POST");
-       // inputs.add(input);
-        resp.setContentType("application/json; charset=utf-8");
-        OutputStream output = resp.getOutputStream();
-       // String json = GSON.toJson(input);
-       // output.write(json.getBytes(StandardCharsets.UTF_8));
-        output.flush();
-        output.close();
+        inputs.add(tmp.split("=")[1]);
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        inputs.add("hello world");
+        System.out.println(inputs);
         String input = GSON.fromJson(req.getReader(), String.class);
         System.out.println(input);
         System.out.println("GET");
--- src/main/webapp/scripts.js
+++ src/main/webapp/scripts.js
@@ -1,3 +1,52 @@
+function goPayment() {
+    let valueRadio = $('input[name="place"]:checked').val();
+    if (valueRadio === null || valueRadio === undefined) {
+        alert("Select a place")
+    } else {
+        let row = valueRadio.split("")[0];
+        let place = valueRadio.split("")[1] ;
+        window.location.href = '/job4j_cinema/payment.html?row=' + row + '&' + 'place=' + place;
+    }
+
+}
+
+
+function sendPay() {
+    let valueRadio = $('input[name="place"]:checked').val();
+    if (valueRadio === null || valueRadio === undefined) {
+        alert("Select a place")
+    } else {
+        postReq(valueRadio)
+
+    }
+}
+
+function postReq(value) {
+
+    $(document).ready(function()
+    {
+        $.ajax({
+            url: "http://localhost:8081/job4j_cinema/pay",
+            dataType: "json",
+            method: "POST",
+            data: {value},
+        }).done(function () {
+
+        }).fail(function (err) {
+            console.log(err)
+        });
+    });
+    window.location.href = '/job4j_cinema/payment.html?place=' + value
+}
+
+ function printHeader(row, place) {
+    $(function () {
+        $('h3').append("<p>Вы выбрали " + row + " ряд " + place + " место, сумма : 500 рублей.</p>");
+    })
+
+ }
+
+
 /*
 
 $(document).ready(function()
